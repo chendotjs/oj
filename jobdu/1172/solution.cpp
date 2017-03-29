7833db79383a73e04436fc2501a1da1268647c2f
chendotjs <chendotjs@gmail.com>
2017-03-29 14:23:29 +0800

    finish huffman

---
+++ jobdu/1172/solution.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+#include <stdio.h>
+#include <vector>
+// #include <functional>
+
+using namespace std;
+priority_queue<int, vector<int>, std::greater<int>> Q;
+
+int main() {
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    while (!Q.empty())
+      Q.pop();
+    for (int i = 0; i < n; i++) {
+      int x;
+      scanf("%d", &x);
+      Q.push(x);
+    }
+    int ans = 0;
+    while (Q.size() > 1) {
+      int a = Q.top();
+      Q.pop();
+      int b = Q.top();
+      Q.pop();
+      ans += (a + b);
+      Q.push(a + b);
+    }
+    printf("%d\n", ans);
+  }
+
+  return 0;
+}
