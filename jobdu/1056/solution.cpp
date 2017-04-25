c77d749fb8264c369bdddf8e1bdc991eebbeabe2
chendotjs <chendotjs@gmail.com>
2017-04-25 10:08:27 +0800

    finish gcd

---
+++ jobdu/1056/solution.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int gcd(int a, int b) {
+    if (a % b == 0) return b;
+    return gcd(b, a % b);
+} 
+
+int main() {
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n", gcd(a, b));
+    }
+}
